9d431b16949f1b03271ded0990d551d8db812db9
tvv89 <tymchuk.volodymyr@gmail.com>
2017-01-16 22:52:24 +0200

    implements Interface for Hotels And User

--- src/FinalProject/Controller.java
+++ src/FinalProject/Controller.java
@@ -10,7 +10,7 @@ import static FinalProject.Hotel.*;
 Объект данного класса иммитирует работу юзера, путем генерации запросов
 */
 
-public class Controller implements UserInterface {
+public class Controller implements UserInterface, HotelsAPI {
     private List<Hotel> hotels = new ArrayList<>();
     //@добавлю DAO на будущее, потом в методах нужно использовать дао вместо поля hotels
     private DAOImpl hotelsDao = new DAOImpl();
--- src/FinalProject/UserInterface.java
+++ src/FinalProject/UserInterface.java
@@ -1,17 +1,10 @@
 package FinalProject;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.UUID;
 
 /**
  * Created by tvv89 on 16.01.2017 for ProgectGojavaGroup-7.
  */
 public interface UserInterface {
-    Collection<Hotel> findHotelByName(String name);
-    Collection<Hotel> findHotelByCity(String city);
-    void bookRoom(UUID roomID, UUID userID, UUID hotelID);
-    void cancelReservation(UUID roomID, UUID userID, UUID hotelID);
-    Collection<Hotel> findRoom(Map<String, String> params);
+
     User registerUser(User user);
 }
