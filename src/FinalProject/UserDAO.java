fcd82a62455fc81e56b9796caa8b3246dd56cfdf
Alex <Basialex55@gmail.com>
2017-01-24 21:27:35 +0200

    t

--- src/FinalProject/UserDAO.java
+++ src/FinalProject/UserDAO.java
@@ -9,21 +9,5 @@ import java.util.Properties;
  * Created by Alex on 24.01.2017.
  */
 public class UserDAO  {
-    private Connection myConn;
-
-    public UserDAO() throws  Exception{
-        Properties prop = new Properties();
-        prop.load(newFileInputStream("info.user"));
-        String name = prop.getProperty("name");
-
-
-        myConn = DriverManager.getConnection("name");
-            }
-
-    private InputStream newFileInputStream(String s) {
-        return null;
-    }
-
-
 
 }
