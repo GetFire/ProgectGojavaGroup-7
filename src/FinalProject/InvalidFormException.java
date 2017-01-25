3831bc4e4a8601c38927a750f5c44b901758c122
Denis Azhymov <azhimov93@gmail.com>
2017-01-25 11:27:27 +0200

    Просто рефактор

--- src/FinalProject/InvalidFormException.java
+++ src/FinalProject/InvalidFormException.java
@@ -1,7 +1,7 @@
 package FinalProject;
 
 /**
- * Created by User on 17.01.2017.
+ * Created by GetFire on 17.01.2017 for ProgectGojavaGroup-7
  */
 public class InvalidFormException extends RuntimeException {
     public InvalidFormException(String message) {
