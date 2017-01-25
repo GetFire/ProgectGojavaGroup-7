a4c5eb22569a9f0c2ec7eabba879de490a2ab869
Denis Azhymov <azhimov93@gmail.com>
2017-01-25 11:26:05 +0200

    Зачем этот интерфейс?

--- src/FinalProject/UserInterface.java
+++ src/FinalProject/UserInterface.java
@@ -4,6 +4,9 @@ package FinalProject;
 /**
  * Created by tvv89 on 16.01.2017 for ProgectGojavaGroup-7.
  */
+
+/**UPD 25.01.17: Для чего этот интерфейс?*/
+
 public interface UserInterface {
 
     User registerUser(User user);
