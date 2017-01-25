20e82af38f1da32d07950745a6e58007e382eb26
tvv89 <tymchuk.volodymyr@gmail.com>
2017-01-25 21:55:02 +0200

    добавлено DAO для управления бронями Order

---
+++ src/FinalProject/OrderDAO.java
@@ -0,0 +1,32 @@
+package FinalProject;
+
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+import java.util.List;
+
+/**
+ * Created by tvv89 on 25.01.2017 for FinalProjectGroup7.
+ */
+public class OrderDAO extends DAOImpl<Order>
+{
+    private List<Order> orders;
+
+    public OrderDAO(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public Order save(Order object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Order remove(Order object) {
+        return super.remove(object);
+    }
+
+    @Override
+    public Order update(Order object) {
+        return super.update(object);
+    }
+}
