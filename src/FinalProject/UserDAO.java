1e5768267630d9c0f6527735dd45a137592abdde
Denis Azhymov <azhimov93@gmail.com>
2017-01-26 14:55:12 +0200

    Новый функционал. Теперь при каждом создании userDAO  загружаются юзеры из файлика. Таким образом мы не теряем наших "старых" юзеров, которыебыли зарегистрированы в предыдущие запуски программы

--- src/FinalProject/UserDAO.java
+++ src/FinalProject/UserDAO.java
@@ -1,5 +1,7 @@
 package FinalProject;
 
+import java.io.*;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -7,10 +9,21 @@ import java.util.Set;
  */
 public class UserDAO extends DAOImpl<User> {
     private Set<User> users;
+    private static final String FILE_DIRECTION = "D:\\practice\\ProgectGojavaGroup-7\\src\\EscortFiles\\Users.txt";
 
-    public UserDAO(Set<User> users) {
-        this.users = users;
+    public UserDAO(Set<User> aUsers) {
+        this.users = downloadUserDAO();
+        aUsers.removeAll(this.users);
+        this.users.addAll(aUsers);
+        writeUserDao(this.users);
     }
+
+    public UserDAO (){
+        this.users = downloadUserDAO();
+    }
+
+
+
 
     @Override
     public User save(User aUser) {
@@ -30,4 +43,47 @@ public class UserDAO extends DAOImpl<User> {
      и сохранять уже обновленного юзера в базу и вернуть его. Если его нет, возвращать sout(" Sorry, user not found") retutn aUser */
         return aUser;
     }
+
+    private Set<User> downloadUserDAO() {
+        Set<User> users = new HashSet<>();
+        BufferedReader br = null;
+        String line;
+        try {
+            br = new BufferedReader(new FileReader(FILE_DIRECTION));
+            while ((line = br.readLine()) != null) {
+                String[] s = line.split("\\\\");
+                users.add(new User(s[0], s[1], s[2]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return users;
+
+    }
+
+    public void writeUserDao(Set<User> user) {
+        StringBuilder content = new StringBuilder();
+        for (User user1 : user) {
+            content.append(user1.getNickname()).append("\\").append(user1.getName()).append("\\").append(user1.getSecondName()).append("\n");
+        }
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(FILE_DIRECTION)));
+            bw.write(content.toString());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null)
+                    bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
 }
+
