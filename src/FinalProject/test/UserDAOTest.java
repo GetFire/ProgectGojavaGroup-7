456fe49c568ccf5d50f82867911bad253abc58a4
tvv89 <tymchuk.volodymyr@gmail.com>
2017-01-27 19:47:03 +0200

    UserDAOTest:  изменено с Set на List

--- src/FinalProject/test/UserDAOTest.java
+++ src/FinalProject/test/UserDAOTest.java
@@ -5,13 +5,15 @@ import FinalProject.main.UserDAO;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.*;
 
 public class UserDAOTest {
-    private static Set<User> users = new HashSet<>();
+    private static List<User> users = new ArrayList<>();
     private static UserDAO userDAO = new UserDAO(users);
 
     @Test
