1edbe7be4a83a2d3d4bc0fb03baafc6ad9a4ac16
tvv89 <tymchuk.volodymyr@gmail.com>
2017-01-30 22:32:39 +0200

    Controller:  в bookRoom ля вывода заказа испоьзовано getNameByID, getCityByID

--- src/FinalProject/Controller/Controller.java
+++ src/FinalProject/Controller/Controller.java
@@ -84,7 +84,7 @@ public class Controller {
             //add order
             orderService.save(new Order(user.getId(), hotelID, roomID, startDate, days));
             System.out.println("Congratulations! You have booked the room!");
-            System.out.println("HotelID: [" + hotelID + "], roomID: [" + roomID + "] from " + startDate + " for " + days + " day(s)");
+            System.out.println("In City: " + hotelService.getCityByID(hotelID) + ", Hotel: " + hotelService.getNameByID(hotelID) + " from " + startDate + " for " + days + " day(s)");
         } else
             System.out.println("Sorry, you should be logged in to continue!");
     }
