782bb22fb984f5bee0f2d18c3c83e11c0c564406
Gluck-mz <aleksey.orishchenko@gmail.com>
2017-01-31 06:33:59 +0100

    added new tests

--- src/FinalProject/test/ProjectUTILSTest.java
+++ src/FinalProject/test/ProjectUTILSTest.java
@@ -6,33 +6,49 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ProjectUTILSTest {
     private static List<Hotel> emptyList = new ArrayList<>();
+    private static String emptyString = "";
+    private static String invalidDate = "Invalid Date";
+    private static String dateFormat1 = "20170202";
+    private static String dateFormat2 = "02-02-2017";
+    private static String dateFormat3 = "02-Feb-2017";
+    private static String dateFormat4 = "02 Feb 2017";
 
-    @Ignore
+    private static Date currentDate = new Date();
+
     @Test
-    public void createUsersRequest() throws Exception {
-        System.out.println("It's too complicated to test Buffered reader. Will google it and we'll see");
+    public void toDate_withNull() throws Exception {
+        assertEquals(ProjectUTILS.toDate(emptyString).compareTo(currentDate), 1);
     }
 
     @Test
-    public void createHotels_withNegativeNumber() throws Exception {
-        assertEquals(emptyList, ProjectUTILS.createHotels());
+    public void toDate_withInvalidDate() throws Exception {
+        assertEquals(ProjectUTILS.toDate(invalidDate).compareTo(currentDate), 1);
     }
 
-    /*@Test
-    public void createDefaultRooms_noHotelsCreated() throws Exception {
-        assertEquals(emptyList, ProjectUTILS.createDefaultRooms(emptyList));
-    }*/
-
-    @Ignore
     @Test
-    public void readOnlyFillLine() throws Exception {
-        System.out.println("Do we really need this method?");
+    public void toDate_withDateFomat1() throws Exception {
+        assertEquals(ProjectUTILS.toDate(dateFormat1).compareTo(currentDate), 1);
     }
 
+    @Test
+    public void toDate_withDateFomat2() throws Exception {
+        assertEquals(ProjectUTILS.toDate(dateFormat2).compareTo(currentDate), 1);
+    }
+
+    @Test
+    public void toDate_withDateFomat3() throws Exception {
+        assertEquals(ProjectUTILS.toDate(dateFormat3).compareTo(currentDate), 1);
+    }
+
+    @Test
+    public void toDate_withDateFomat4() throws Exception {
+        assertEquals(ProjectUTILS.toDate(dateFormat4).compareTo(currentDate), 1);
+    }
 }
